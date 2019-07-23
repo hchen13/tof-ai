7d2803e2863f497f7849d694494d4574941a9e4b
dohpiny <duhepin@fishsaying.com>
2019-07-23 14:03:48 +0800

    app update

---
+++ app/README.md
@@ -0,0 +1,4 @@
+
+  ```
+  http-server --cert ./key/server.crt --key ./key/server.key --ssl --cors -p 443
+  ```
---
+++ app/work.js
@@ -0,0 +1,10 @@
+onmessage = function (image,model) {
+  //从1加到num
+  // var num = event.data;
+  // var result = 0;
+  // for (var i = 1; i <= num; i++) {
+  //     result += i;
+  // }
+  console.log(image,model);
+  postMessage(result);
+}
