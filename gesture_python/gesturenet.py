0fda227a12e8575f0c0a38b6bc492a1990c84bb2
Ethan Chen <gradschool.hchen13@gmail.com>
2019-07-24 01:15:47 +0800

    add gesturenet model definition

---
+++ gesture_python/gesturenet.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.python.keras import Model
+from tensorflow.python.keras.layers import Flatten, GlobalAveragePooling2D, GlobalMaxPooling2D, Dense
+
+
+class GestureNet:
+    def __init__(self, num_classes, dense_layers=None, input_shape=(224, 224, 3), top_method='flatten', mobilenet_params=None):
+        if dense_layers is None:
+            dense_layers = []
+        if mobilenet_params is None:
+            mobilenet_params = {}
+
+        base_model = self._get_base_model(input_shape, **mobilenet_params)
+        bottleneck = base_model.output
+        x = {
+            'flatten': Flatten(),
+            'avg': GlobalAveragePooling2D(),
+            'max': GlobalMaxPooling2D()
+        }[top_method](bottleneck)
+
+        for units in dense_layers:
+            init = tf.keras.initializers.VarianceScaling()
+            x = Dense(units, activation='relu', kernel_initializer=init)(x)
+
+        init = tf.keras.initializers.VarianceScaling()
+        predictions = Dense(num_classes, activation='softmax', kernel_initializer=init, use_bias=False)(x)
+        self.model = Model(base_model.input, predictions)
+
+    def _get_base_model(self, input_shape, **params):
+        _base = tf.keras.applications.MobileNetV2
+        return _base(include_top=False, input_shape=input_shape, **params)
+
+
+if __name__ == '__main__':
+
+    mobilenet_params = {
+        'alpha': 1.,
+        'depth_multiplier': 1,
+    }
+
+    engine = GestureNet(5, [100])
+    engine.model.summary()
